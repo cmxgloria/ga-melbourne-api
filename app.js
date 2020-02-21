9de8ee60c3c26cd4af320f2620c28c0401f207a2
Gloria Chen <cmxgloria@gmail.com>
2020-02-21 11:41:18 +1100

    updated map sample

--- app.js
+++ app.js
@@ -18,6 +18,36 @@ const results = [
     location: { lat: -37.82064, long: 144.958325 }
   }
 ];
+// lat, log;
+
+// function GetMap(location_lat, location_log) {
+//   // map id from div
+//   var map = new Microsoft.Maps.Map("#map", {
+//     center: new Microsoft.Maps.Location(location_lat, location_log),
+//     zoom: 17
+//   });
+
+//   function addPin(pin_lat, pin_log) {
+//     //Create custom Pushpin
+//     var pin = new Microsoft.Maps.Pushpin(
+//       { latitude: pin_lat, longitude: pin_log },
+//       {
+//         title: document.querySelector("#city").textContent,
+//         subTitle: "Dream dist",
+//         text: "1"
+//       }
+//     );
+//     //Add the pushpin to the map
+//     map.entities.push(pin);
+//   }
+//   // run
+//   addPin(location_lat, location_log);
+// }
+
+// GetMap(lat, log);
+
+// console.log(GetMap(-37.818555, 144.959076));
+
 // callback=GetMap from script in html
 function GetMap() {
   console.log("khkh");
@@ -43,27 +73,6 @@ function GetMap() {
   // run
   results.forEach(addPin);
 }
-
-// var results = [
-//   {
-//     name: "Melbourne Cricket Grounds",
-//     location: { lat: -37.819967, long: 144.983449 }
-//   },
-//   { name: "Flagstaff Gardens", location: { lat: -37.81068, long: 144.954352 } },
-//   {
-//     name: "Emporium Melbourne",
-//     location: { lat: -37.812433, long: 144.963787 }
-//   },
-//   { name: "City Library", location: { lat: -37.817039, long: 144.965983 } },
-//   {
-//     name: "Southern Cross Station",
-//     location: { lat: -37.818281, long: 144.952776 }
-//   },
-//   {
-//     name: "Sea Life Melbourne Aquarium",
-//     location: { lat: -37.82064, long: 144.958325 }
-//   }
-// ];
 
 // var center = function(arr) {
 //   var minX, maxX, minY, maxY;
